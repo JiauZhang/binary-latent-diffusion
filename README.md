8b3c9617b16bfaa189609f156de22e023d5d2246
JiauZhang <1743960454@qq.com>
2023-04-19 21:54:40 +0800

    basic VQModel training script

--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 Implementation of [Binary Latent Diffusion](https://arxiv.org/abs/2304.04820).
 
 ```shell
-pip install datasets lossers
+pip install datasets lossers==0.0.6
 ```
+
+| input image | reconstruction | iterations | loss |
+| :---------: | :------------: | :--------: | :--: |
+| <img src="./sample/input.png" width="268" alt="input image"/> | <img src="./sample/rec-017000.png" width="268" alt="reconstruction image"/> | 017000 | LPIPS |
--- train_vq.py
+++ train_vq.py
@@ -1,16 +1,59 @@
+from tqdm import tqdm
+from torch import nn, optim
+import torch, argparse, math
 from vq_model import VQModel
 from lossers.lpips import LPIPS
-from torch import nn, optim
-import torch
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from torchvision import transforms, utils
+from torchvision.transforms import functional as F
 
-model = VQModel()
-loss_fn = LPIPS(net='vgg')
+def sample_data(loader):
+    while True:
+        for batch in loader:
+            yield batch
+
+parser = argparse.ArgumentParser(description="Train VQModel")
+parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument("--dataset", type=str, default="lambdalabs/pokemon-blip-captions")
+parser.add_argument("--cache_dir", type=str, default="./.cache")
+parser.add_argument("--iter", type=int, default=800000, help="total training iterations")
+parser.add_argument("--batch", type=int, default=64, help="batch sizes for each gpus")
+parser.add_argument("--size", type=int, default=64, help="image sizes for the model")
+args = parser.parse_args()
+
+model = VQModel().to(args.device)
+lpips = LPIPS(net='vgg').to(args.device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.999))
-input = torch.randn((1, 3, 32, 32), requires_grad=False)
 
-for i in range(10):
-    loss = loss_fn(input, model(input))
+to_tensor = transforms.Compose([
+    transforms.Resize(args.size),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5), inplace=True),
+])
+def preprocess(data):
+    data['image'][0] = to_tensor(data['image'][0]).to(args.device)
+    return data
+dataset = load_dataset(args.dataset, split="train", cache_dir=args.cache_dir)
+dataset = dataset.with_transform(preprocess)
+dataloader = DataLoader(dataset, batch_size=args.batch, shuffle=True, drop_last=True)
+dataloader = sample_data(dataloader)
+pbar = tqdm(range(args.iter))
+sample = next(dataloader)['image']
+
+for idx in pbar:
+    image = next(dataloader)['image']
+    rec = model(image)
+    loss = lpips(image, rec).mean()
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-    print(loss.item())
+    pbar.set_description((f"loss: {loss.item():.4f}"))
+
+    if idx % 200 == 0:
+        with torch.no_grad():
+            rec = model(sample)
+            utils.save_image(
+                rec, f"sample/{str(idx).zfill(6)}.png",
+                nrow=int(math.sqrt(args.batch)), normalize=True, value_range=(-1, 1),
+            )
