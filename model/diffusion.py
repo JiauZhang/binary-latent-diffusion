d98f8fc1a5996fe266eafbe44d962c5ee3249144
JiauZhang <1743960454@qq.com>
2023-05-16 22:06:38 +0800

    fix incorrect beta_t

--- model/diffusion.py
+++ model/diffusion.py
@@ -12,9 +12,9 @@ class UNet(nn.Module):
     def register_schedule(self, timesteps=64):
         b_t = torch.linspace(0, 0.5, timesteps, dtype=torch.float64)
         beta_t = (b_t[1:] - b_t[:-1]) / (0.5 - b_t[:-1])
+        zero = torch.zeros(1)
+        beta_t = torch.cat([zero, beta_t])
         k_t = torch.cumprod(1 - beta_t, dim=0)
-        one = torch.ones(1)
-        k_t = torch.cat([one, k_t])
 
         self.register_buffer('b_t', b_t)
         self.register_buffer('beta_t', beta_t)
